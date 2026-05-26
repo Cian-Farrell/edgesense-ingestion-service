0bfc1c9bccb88958f373fd0df333c8977e9dd1c4
Cian-Farrell <cian.farrell41@gmail.com>
2026-05-26 10:23:44 +0100

    Add StorageServiceClient - forward sensor data to storage service

--- src/main/java/com/edgesense/ingestion/SensorDataListener.java
+++ src/main/java/com/edgesense/ingestion/SensorDataListener.java
@@ -17,6 +17,12 @@ public class SensorDataListener {
     private static final String KEY_PATH = "C:/Users/cianf/InternalPlacement/certs/86f52f8b5a613ca70021e08a299db7a2a110aeb70eef4239b4615a4ab88befd6-private.pem.key";
     private static final String CA_PATH = "C:/Users/cianf/InternalPlacement/certs/AmazonRootCA1.pem";
 
+    private final StorageServiceClient storageServiceClient;
+
+    public SensorDataListener(StorageServiceClient storageServiceClient) {
+        this.storageServiceClient = storageServiceClient;
+    }
+
     @PostConstruct
     public void connect() throws Exception{
         MqttClientConnection connection = AwsIotMqttConnectionBuilder
@@ -33,6 +39,7 @@ public class SensorDataListener {
         connection.subscribe(TOPIC, QualityOfService.AT_LEAST_ONCE, (message) -> {
             String payload = new String(message.getPayload(), StandardCharsets.UTF_8);
             System.out.println("Received sensor data: " + payload);
+            storageServiceClient.saveSensorReading(payload);
         }).get();
 
         System.out.println("Subscribed to topic: " + TOPIC);
