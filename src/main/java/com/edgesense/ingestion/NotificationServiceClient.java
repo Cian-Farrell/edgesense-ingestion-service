faa523fb634a10e57aa1d1c01a328e15ddedb0c8
Cian-Farrell <cian.farrell41@gmail.com>
2026-08-15 11:59:01 +0100

    Fix

--- src/main/java/com/edgesense/ingestion/NotificationServiceClient.java
+++ src/main/java/com/edgesense/ingestion/NotificationServiceClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class NotificationServiceClient {
 
-    private static final String NOTIFICATION_SERVICE_URL = "http://172.31.30.53:8083/api/notifications/anomaly";
+    private static final String NOTIFICATION_SERVICE_URL = "http://notification.edgesense.local:8083/api/notifications/anomaly";
     private final RestTemplate restTemplate;
 
     public NotificationServiceClient() {
